8a2ef2735aa6ccb29e833be2e237a9a3ef6a62f8
yasuesou <86512554+yasuesou@users.noreply.github.com>
2021-06-26 20:05:40 +0900

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 // Change the url to the domain of your app
 exports.url = 'https://yasuesousg.herokuapp.com/';
 
-exports.senderEmail = "sender@example.com";
-exports.senderName = "Sender Name";
+exports.senderEmail = "info@yasuesou.com";
+exports.senderName = "”ªõŒb‘‘‚Ìƒ[ƒ‹ƒ}ƒKƒWƒ“";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
